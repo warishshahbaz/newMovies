0b7e1006fbf2d3bca273c738b01f0cd312bae710
md shahbaz warish <mdshahbazwarish1996@gmail.com>
2022-10-17 19:03:49 +0530

    nav modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,57 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {swal} from 'sweetalert'
+import React from "react";
+import { Link } from "react-router-dom";
+import { swal } from "sweetalert";
+import { useAuth0 } from "@auth0/auth0-react";
 
-function Header({inputHandle,inputData}) {
+function Header({ inputHandle, inputData }) {
+  const { loginWithRedirect,isAuthenticated,logout,user } = useAuth0();
+  const upComingMovies = () => {
+    alert("Wait for few days...");
+  };
 
-    const upComingMovies = ()=>{
-        alert('Wait for few days...')
+  
+  return (
+    <>
+      <header>
+        <nav>
+          <div className="log">
+            <img
+              src="https://static8.depositphotos.com/1378583/987/v/600/depositphotos_9878369-stock-illustration-camera-and-film-logo.jpg"
+              alt="logo"
+            />
+             
+          </div>
         
-    }
-    return (
-        <>
-            <header>
-                <nav>
-                    <div className="log">
-                        <img src="https://static8.depositphotos.com/1378583/987/v/600/depositphotos_9878369-stock-illustration-camera-and-film-logo.jpg" alt="logo" /></div>
-                    <div className="field"><input type="text" placeholder='search..' onChange={inputHandle} value={inputData} /></div>
-                </nav>
-                <div className="upcoming">
-                    <h3 className='movies_name'>Captain America: The First Avenger (1942)</h3>
-                    <button onClick={upComingMovies} >Up Coming ...</button>
-                </div>
-            </header>
-        </>
-    )
+        
+          <div className="field">
+            <input
+              type="text"
+              placeholder="search.."
+              onChange={inputHandle}
+              value={inputData}
+            />
+          </div>
+          <div className="login_btn" >
+          {
+            isAuthenticated ? 
+            <button onClick={() => logout({ returnTo: window.location.origin })}> Log Out</button> :
+             <button onClick={() => loginWithRedirect()}>Log In</button>}
+             <div>
+              {
+                isAuthenticated && <p style={{color:"grey"}} >{(user.name.slice(0,-10))}...</p>
+              }
+             </div>
+          </div>
+        </nav>
+        <div className="upcoming">
+          <h3 className="movies_name">
+            Captain America: The First Avenger (1942)
+          </h3>
+          <button onClick={upComingMovies}>Up Coming ...</button>
+        </div>
+      </header>
+    </>
+  );
 }
 
-export default Header
+export default Header;
--- src/index.js
+++ src/index.js
@@ -4,15 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+  domain="dev-vgmidjjv.us.auth0.com"
+  clientId="FcrZbh8r3flQc6taglYTK3QZpfNpvNIv"
+  redirectUri={window.location.origin}
+>
   <BrowserRouter>
     <App />
   </BrowserRouter>
   
-  </React.StrictMode>
+</Auth0Provider>,
+ 
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
